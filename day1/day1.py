d9569530636fe026a816c71fe4391d769e109ac9
Ernest Feret <ernest.feret@gmail.com>
2019-11-24 11:56:03 +0100

    total mass for fuel calculations

--- day1/day1.py
+++ day1/day1.py
@@ -10,22 +10,32 @@ def resolve_total_fuel(mass):
     Resolves amount of fuel for mass plus extra fuel that is taking
     into consideration fuel mass itself.
     """
-    pass
+    total_fuel = 0
+
+    while (mass := resolve_fuel(mass)) > 0:
+        total_fuel += mass
+
+    return total_fuel
 
 
 def generate_numbers(data):
     yield from (int(num.strip()) for num in data if num)
 
 
-def calculate_mass_fuel(data):
+def calculate_mass_fuel(data, with_extra_mass=False):
     elements_mass = generate_numbers(data)
-    return sum(map(resolve_fuel, elements_mass))
+    fuel_calc = resolve_total_fuel if with_extra_mass else resolve_fuel
+
+    return sum(map(fuel_calc, elements_mass))
 
 
 def main():
     input_data = open('input')
     basic_fuel_mass = calculate_mass_fuel(input_data)
+    input_data.seek(0)
+    total_fuel_mass = calculate_mass_fuel(input_data, with_extra_mass=True)
     print(f"{basic_fuel_mass=}")
+    print(f"{total_fuel_mass=}")
 
 
 if __name__ == "__main__":
--- day1/tests.py
+++ day1/tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from day1 import resolve_fuel, calculate_mass_fuel
+from day1 import resolve_fuel, resolve_total_fuel, calculate_mass_fuel
 
 
 def test_12_resolves_to_2():
@@ -18,6 +18,18 @@ def test_100756_resolves_to_33583():
     assert resolve_fuel(100756) == 33583
 
 
+def test_resolve_total_fuel_14_to_2():
+    assert resolve_total_fuel(14) == 2
+
+
+def test_resolve_total_fuel_1969_to_966():
+    assert resolve_total_fuel(1969) == 966
+
+
+def test_resolve_total_fuel_100756_to_50346():
+    assert resolve_total_fuel(100756) == 50346
+
+
 def test_correct_result():
     data = open('input')
     assert calculate_mass_fuel(data) == 3282935
@@ -28,4 +40,9 @@ if __name__ == "__main__":
     test_14_resolves_to_2()
     test_1969_resolves_to_654()
     test_100756_resolves_to_33583()
+    test_resolve_total_fuel_14_to_2
+    test_resolve_total_fuel_1969_to_966()
+    test_resolve_total_fuel_100756_to_50346()
+
     test_correct_result()
+
