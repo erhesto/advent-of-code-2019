7b36795c92178c3fc693220a4f11c0e3a53a82b1
Ernest Feret <ernest.feret@gmail.com>
2019-11-24 12:33:51 +0100

    added typing to fuel calc

--- day1/fuelcalulation.py
+++ day1/fuelcalulation.py
@@ -1,11 +1,14 @@
-def resolve_fuel(mass):
+from typing import List
+
+
+def resolve_fuel(mass: int) -> int:
     """
     Resolves amount of fuel for given mass.
     """
     return mass // 3 - 2
 
 
-def resolve_total_fuel(mass):
+def resolve_total_fuel(mass: int) -> int:
     """
     Resolves amount of fuel for mass plus extra fuel that is taking
     into consideration fuel mass itself.
@@ -18,7 +21,7 @@ def resolve_total_fuel(mass):
     return total_fuel
 
 
-def calculate_mass_fuel(elements_mass, with_extra_mass=False):
+def calculate_mass_fuel(elements_mass: List[int], with_extra_mass: bool = False) -> int:
     fuel_calculation_method = resolve_total_fuel if with_extra_mass else resolve_fuel
 
     return sum(map(fuel_calculation_method, elements_mass))
