059fd19168fc9126cac62eed6995a442f2a82c03
Ernest Feret <ernest.feret@gmail.com>
2019-12-20 22:53:55 +0100

    first parts of day2 finished

--- day2/run.c
+++ day2/run.c
@@ -37,22 +37,66 @@ int *create_array_from_string(char *input, char delim, int arr_sz) {
 	return arr_ptr;
 }
 
+int add(int x, int y) {
+	return x + y;
+}
+
+int mul(int x, int y) {
+	return x * y;
+}
+
+int compute_result(int *arr) {
+	int start_pos = 0;
+	int first_num, second_num;
+	int output_index;
+	int method = arr[start_pos];
+	int (*method_ptr)(int, int);
+
+	while (method != 99) { 
+		switch (method) {
+			case 1:
+				method_ptr = add;
+				break;
+			case 2:
+				method_ptr = mul;
+				break;
+			default:
+				printf("Something went wrong!");
+				exit(0);
+				break;
+		}
+
+		// get the numbers
+		first_num = arr[arr[start_pos + 1]];
+		second_num = arr[arr[start_pos + 2]];
+		output_index = arr[start_pos + 3];
+
+		// compute the result to the original arr
+		arr[output_index] = method_ptr(first_num, second_num);
+		
+		// move to another four numbers
+		start_pos += 4;
+		method = arr[start_pos];
+	}
+
+	return arr[0];
+}
+
 
 int main(int argc, char *argv[]) {
 	char buff[BUFF_SIZE];
 	char delim = ',';
 	int *arr;
 	int arr_sz;
+	int result;
 	FILE *f = fopen("input", "r");
 	fgets(buff, BUFF_SIZE, f);
 
 	arr_sz = count_delim(buff, delim);
 	arr = create_array_from_string(buff, delim, arr_sz);
-	
-	for (int i = 0; i < arr_sz; i++) {
-		printf("%d\n", arr[i]);
-	}
 
+	result = compute_result(arr);
+	printf("Result: %d\n", result);
 	free(arr);
 	fclose(f);
 
